99bca08b4beccd265f0ba485bce35ff2051a12c7
Vedran Krivokuca <vedran@krivokuca.dev>
2024-03-17 18:47:35 +0100

    Rename  to

--- semver.py
+++ semver.py
@@ -2,13 +2,13 @@ from typing import Iterator, Union, List
 from typing import Tuple
 
 
-class Version:
-    """
+class BaseVersion:
+    """BaseVersion classMinimal Semantic Versioning library
     This is a minimal Python Semantic Version library.
 
     Values can be passed as arbitrary number of `int` numbers, for example:
 
-    >>> v = Version(1, 0, 0)
+    >>> v = BaseVersion(1, 0, 0)
     >>> str(v)
     '1.0.0'
 
@@ -18,29 +18,29 @@ class Version:
     >>> str(v)
     '1.0.0.5322'
     >>> v
-    Version(1, 0, 0, 5322)
+    BaseVersion(1, 0, 0, 5322)
 
     Implementation of comparison operators (`__lt__`, `__gt__`, `__le__`, `__ge__`) allows for
     sorting operations to work. For example:
 
     >>> versionslist = [
-    ...     Version.from_string("1.0.0"), Version.from_string("1.0.1"),
-    ...     Version.from_string("0.9.95"), Version(0, 8, 97),
-    ...     Version(2024, 2, 25, 101), Version(1, 0, 0, 0)]
+    ...     BaseVersion.from_string("1.0.0"), BaseVersion.from_string("1.0.1"),
+    ...     BaseVersion.from_string("0.9.95"), BaseVersion(0, 8, 97),
+    ...     BaseVersion(2024, 2, 25, 101), BaseVersion(1, 0, 0, 0)]
     >>> sorted(versionslist)
-    [Version(0, 8, 97), Version(0, 9, 95), Version(1, 0, 0), Version(1, 0, 0, 0), Version(1, 0, 1), Version(2024, 2, 25, 101)]
+    [BaseVersion(0, 8, 97), BaseVersion(0, 9, 95), BaseVersion(1, 0, 0), BaseVersion(1, 0, 0, 0), BaseVersion(1, 0, 1), BaseVersion(2024, 2, 25, 101)]
     >>> sorted(versionslist, reverse=True)
-    [Version(2024, 2, 25, 101), Version(1, 0, 1), Version(1, 0, 0, 0), Version(1, 0, 0), Version(0, 9, 95), Version(0, 8, 97)]
+    [BaseVersion(2024, 2, 25, 101), BaseVersion(1, 0, 1), BaseVersion(1, 0, 0, 0), BaseVersion(1, 0, 0), BaseVersion(0, 9, 95), BaseVersion(0, 8, 97)]
 
 
     Some possible Edge cases tests:
 
     Open tuple can give weird representation, but logic still works:
 
-    >>> version_edgecase = Version(1,)
-    >>> version_base = Version(1)
+    >>> version_edgecase = BaseVersion(1,)
+    >>> version_base = BaseVersion(1)
     >>> version_edgecase
-    Version(1,)
+    BaseVersion(1,)
     >>> len(version_edgecase)
     1
     >>> version_edgecase == version_base
@@ -52,11 +52,11 @@ class Version:
 
     You can make your version *really* long. Why tho? No idea.
 
-    >>> version_really_long = Version(*[x for x in range(9999999)])
+    >>> version_really_long = BaseVersion(*[x for x in range(9999999)])
     >>> len(version_really_long)
     9999999
     >>> version_really_long  # doctest: +ELLIPSIS
-    Version(0, 1, 2, ..., 9999997, 9999998)
+    BaseVersion(0, 1, 2, ..., 9999997, 9999998)
     >>> version_really_long[-1:]
     (9999998,)
     >>> str(version_really_long)  # doctest: +ELLIPSIS
@@ -64,9 +64,9 @@ class Version:
 
     Version parts can be... negative. I am going to rethink this life choice.
 
-    >>> version_with_negative = Version(-3, 5, -99)
+    >>> version_with_negative = BaseVersion(-3, 5, -99)
     >>> version_with_negative
-    Version(-3, 5, -99)
+    BaseVersion(-3, 5, -99)
     >>> str(version_with_negative)
     '-3.5.-99'
     """
@@ -74,9 +74,9 @@ class Version:
     def __init__(self, *args: int) -> None:
         """Constructor
 
-        >>> v = Version(2023, 3, 5)
+        >>> v = BaseVersion(2023, 3, 5)
         >>> v
-        Version(2023, 3, 5)
+        BaseVersion(2023, 3, 5)
         """
         self.__fill_parts(*args)
 
@@ -84,7 +84,7 @@ class Version:
         """
         Fills out all version parts into class' internal tuple
 
-        >>> v = Version("a", 3, 4)  # doctest: +ELLIPSIS
+        >>> v = BaseVersion("a", 3, 4)  # doctest: +ELLIPSIS
         Traceback (most recent call last):
             ...
         TypeError: Parts of semantic version must be all integer, 'a' passed
@@ -99,24 +99,24 @@ class Version:
         self.__version_parts: Tuple[int, ...] = tuple(__version_parts)
 
     @staticmethod
-    def from_string(version: str) -> "Version":
+    def from_string(version: str) -> "BaseVersion":
         """Create semantic version directly from the string
 
         >>> vs = Version.from_string("2023.03.05")
         >>> vs  # doctest: +ELLIPSIS
-        Version(2023, 3, 5)
+        BaseVersion(2023, 3, 5)
         >>> v = Version.from_string("3.4.5.abc")  # doctest: +ELLIPSIS
         Traceback (most recent call last):
             ...
         ValueError: invalid literal for int() with base 10: 'abc'
         """
         parts: list = [int(v) for v in version.split(".")]
-        return Version(*parts)
+        return BaseVersion(*parts)
 
     def __iter__(self) -> Iterator[int]:
         """Iterate through version parts, used for casting into list, tuple, etc...
 
-        >>> v = Version(2023, 3, 5)
+        >>> v = BaseVersion(2023, 3, 5)
         >>> version_parts = tuple(v)
         >>> version_parts
         (2023, 3, 5)
@@ -127,9 +127,9 @@ class Version:
     def __len__(self) -> int:
         """Returns number of version items
 
-        >>> len(Version(1, 2, 3))
+        >>> len(BaseVersion(1, 2, 3))
         3
-        >>> len(Version(1))
+        >>> len(BaseVersion(1))
         1
         >>> len(Version.from_string("2023.03.04.05555.0"))
         5
@@ -139,7 +139,7 @@ class Version:
     def __str__(self) -> str:
         """String representation of semantic version
 
-        >>> v = Version(2023, 3, 5)
+        >>> v = BaseVersion(2023, 3, 5)
         >>> vs = Version.from_string("2023.03.05")
         >>> str(vs), str(v)
         ('2023.3.5', '2023.3.5')
@@ -149,10 +149,10 @@ class Version:
     def __eq__(self, b: object) -> bool:
         """Implementation of "==" internal type method for comparing values of same type
 
-        >>> version_base = Version(2023, 3, 5)
-        >>> version_short = Version(4)
+        >>> version_base = BaseVersion(2023, 3, 5)
+        >>> version_short = BaseVersion(4)
         >>> version_alternate = Version.from_string("2024.03.05")
-        >>> version_with_zero = Version(2023, 3, 5, 0)
+        >>> version_with_zero = BaseVersion(2023, 3, 5, 0)
         >>> version_same = Version.from_string("2023.03.05")
         >>> version_base == version_alternate
         False
@@ -176,16 +176,16 @@ class Version:
     def __gt__(self, b: object) -> bool:
         """Implementation of ">" internal type method for comparing values of same type
 
-        >>> version_base = Version(2023, 3, 5)
-        >>> version_short = Version(4)
-        >>> version_greater_1 = Version(2023, 3, 5, 0)
-        >>> version_greater_2 = Version(2024, 3, 5)
-        >>> version_greater_3 = Version(2023, 3, 6)
+        >>> version_base = BaseVersion(2023, 3, 5)
+        >>> version_short = BaseVersion(4)
+        >>> version_greater_1 = BaseVersion(2023, 3, 5, 0)
+        >>> version_greater_2 = BaseVersion(2024, 3, 5)
+        >>> version_greater_3 = BaseVersion(2023, 3, 6)
         >>> version_base > version_greater_1, version_base > version_greater_2, version_base > version_greater_3
         (False, False, False)
-        >>> version_lesser_1 = Version(2023, 3, 4)
-        >>> version_lesser_2 = Version(2023, 3)
-        >>> version_lesser_3 = Version(2023, 1, 5)
+        >>> version_lesser_1 = BaseVersion(2023, 3, 4)
+        >>> version_lesser_2 = BaseVersion(2023, 3)
+        >>> version_lesser_3 = BaseVersion(2023, 1, 5)
         >>> version_base > version_lesser_1, version_base > version_lesser_2, version_base > version_lesser_3
         (True, True, True)
         >>> version_same = Version.from_string("2023.3.5")
@@ -209,16 +209,16 @@ class Version:
     def __lt__(self, b: object) -> bool:
         """Implementation of "<" internal type method for comparing values of same type
 
-        >>> version_base = Version(2023, 3, 5)
-        >>> version_short = Version(4)
-        >>> version_greater_1 = Version(2023, 3, 5, 0)
-        >>> version_greater_2 = Version(2024, 3, 5)
-        >>> version_greater_3 = Version(2023, 3, 6)
+        >>> version_base = BaseVersion(2023, 3, 5)
+        >>> version_short = BaseVersion(4)
+        >>> version_greater_1 = BaseVersion(2023, 3, 5, 0)
+        >>> version_greater_2 = BaseVersion(2024, 3, 5)
+        >>> version_greater_3 = BaseVersion(2023, 3, 6)
         >>> version_base < version_greater_1, version_base < version_greater_2, version_base < version_greater_3
         (True, True, True)
-        >>> version_lesser_1 = Version(2023, 3, 4)
-        >>> version_lesser_2 = Version(2023, 3)
-        >>> version_lesser_3 = Version(2023, 1, 5)
+        >>> version_lesser_1 = BaseVersion(2023, 3, 4)
+        >>> version_lesser_2 = BaseVersion(2023, 3)
+        >>> version_lesser_3 = BaseVersion(2023, 1, 5)
         >>> version_base < version_lesser_1, version_base < version_lesser_2, version_base < version_lesser_3
         (False, False, False)
         >>> version_same = Version.from_string("2023.3.5")
@@ -242,16 +242,16 @@ class Version:
     def __ge__(self, b: object) -> bool:
         """Implementation of ">=" internal type method for comparing values of same type
 
-        >>> version_base = Version(2023, 3, 5)
-        >>> version_short = Version(4)
-        >>> version_greater_1 = Version(2023, 3, 5, 0)
-        >>> version_greater_2 = Version(2024, 3, 5)
-        >>> version_greater_3 = Version(2023, 3, 6)
+        >>> version_base = BaseVersion(2023, 3, 5)
+        >>> version_short = BaseVersion(4)
+        >>> version_greater_1 = BaseVersion(2023, 3, 5, 0)
+        >>> version_greater_2 = BaseVersion(2024, 3, 5)
+        >>> version_greater_3 = BaseVersion(2023, 3, 6)
         >>> version_base >= version_greater_1, version_base >= version_greater_2, version_base >= version_greater_3
         (False, False, False)
-        >>> version_lesser_1 = Version(2023, 3, 4)
-        >>> version_lesser_2 = Version(2023, 3)
-        >>> version_lesser_3 = Version(2023, 1, 5)
+        >>> version_lesser_1 = BaseVersion(2023, 3, 4)
+        >>> version_lesser_2 = BaseVersion(2023, 3)
+        >>> version_lesser_3 = BaseVersion(2023, 1, 5)
         >>> version_base >= version_lesser_1, version_base >= version_lesser_2, version_base >= version_lesser_3
         (True, True, True)
         >>> version_same = Version.from_string("2023.3.5")
@@ -275,16 +275,16 @@ class Version:
     def __le__(self, b: object) -> bool:
         """Implementation of "<=" internal type method for comparing values of same type
 
-        >>> version_base = Version(2023, 3, 5)
-        >>> version_short = Version(4)
-        >>> version_greater_1 = Version(2023, 3, 5, 0)
-        >>> version_greater_2 = Version(2024, 3, 5)
-        >>> version_greater_3 = Version(2023, 3, 6)
+        >>> version_base = BaseVersion(2023, 3, 5)
+        >>> version_short = BaseVersion(4)
+        >>> version_greater_1 = BaseVersion(2023, 3, 5, 0)
+        >>> version_greater_2 = BaseVersion(2024, 3, 5)
+        >>> version_greater_3 = BaseVersion(2023, 3, 6)
         >>> version_base <= version_greater_1, version_base <= version_greater_2, version_base <= version_greater_3
         (True, True, True)
-        >>> version_lesser_1 = Version(2023, 3, 4)
-        >>> version_lesser_2 = Version(2023, 3)
-        >>> version_lesser_3 = Version(2023, 1, 5)
+        >>> version_lesser_1 = BaseVersion(2023, 3, 4)
+        >>> version_lesser_2 = BaseVersion(2023, 3)
+        >>> version_lesser_3 = BaseVersion(2023, 1, 5)
         >>> version_base <= version_lesser_1, version_base <= version_lesser_2, version_base <= version_lesser_3
         (False, False, False)
         >>> version_same = Version.from_string("2023.3.5")
@@ -310,7 +310,7 @@ class Version:
 
         >>> v = Version.from_string("1.0.0.5322")
         >>> v
-        Version(1, 0, 0, 5322)
+        BaseVersion(1, 0, 0, 5322)
         """
         return f"Version{self.__version_parts}"
 
@@ -330,7 +330,7 @@ class Version:
         5
         >>> v[-4]
         2023
-        >>> v = Version(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16)
+        >>> v = BaseVersion(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16)
         >>> k = slice(3, 15, 2)
         >>> v[k]
         (4, 6, 8, 10, 12, 14)
